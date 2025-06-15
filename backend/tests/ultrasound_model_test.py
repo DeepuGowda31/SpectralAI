9f6900e93dc8f155561d7b7dd680ed6192a01cf7
DeepuGowda31 <deepugowda.0331@gmail.com>
2025-06-15 13:37:46 +0530

    minor fix

--- backend/tests/ultrasound_model_test.py
+++ backend/tests/ultrasound_model_test.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-
+  
 # Ensure backend/ is on the import path
 root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
